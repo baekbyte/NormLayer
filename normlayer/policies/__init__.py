d91817c408b05b7c5b91885a15de0ba5abaebe5e
Seokhyun (Nathan) Baek <73371470+baekbyte@users.noreply.github.com>
2026-03-03 11:58:56 -0500

    Initializing policies

--- normlayer/policies/__init__.py
+++ normlayer/policies/__init__.py
@@ -4,6 +4,8 @@ from normlayer.policies.coalition_consistency import CoalitionConsistency
 from normlayer.policies.escalate_on_conflict import EscalateOnConflict
 from normlayer.policies.loop_detection import LoopDetection
 from normlayer.policies.no_deception import NoDeception
+from normlayer.policies.no_unsanctioned_action import NoUnsanctionedAction
+from normlayer.policies.norm_conflict_resolution import NormConflictResolution
 from normlayer.policies.response_proportionality import ResponseProportionality
 from normlayer.policies.role_respect import RoleRespect
 
@@ -14,4 +16,6 @@ __all__ = [
     "EscalateOnConflict",
     "ResponseProportionality",
     "CoalitionConsistency",
+    "NormConflictResolution",
+    "NoUnsanctionedAction",
 ]
